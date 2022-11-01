49be9616df06652f2deab8c18ba632b54db8f339
bepuca <bernatpuig@gmail.com>
2022-11-01 14:59:23 +0000

    Add missing sigmoid

--- yolov7/models/yolo.py
+++ yolov7/models/yolo.py
@@ -96,6 +96,7 @@ class Yolov7Model(nn.Module):
             fpn_head_preds[..., [PredIdx.CX, PredIdx.CY]] *= self.detection_head.stride[layer_idx]  # -> Image coordinates
             # TODO: Probably can do it in a more standardized way
             fpn_head_preds[..., [PredIdx.W, PredIdx.H]] *= self.detection_head.anchor_grid[layer_idx] # Anchor box corrections -> Image coordinates
+            fpn_head_preds[..., PredIdx.OBJ:].sigmoid_()
             # TODO: Check if view is needed
             all_preds.append(fpn_head_preds.view(batch_size, -1, self.detection_head.no))
             # TODO: Before there was a .view(bs, -1, self.detection_head.no) in preds, check it

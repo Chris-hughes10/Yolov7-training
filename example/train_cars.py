051059cca95ac3750b9c178139c6036755ef92c3
Chris Hughes <31883449+Chris-hughes10@users.noreply.github.com>
2022-11-02 10:51:30 +0000

    merge cars example

--- example/train_cars.py
+++ example/train_cars.py
@@ -1,19 +1,20 @@
 import os
+import random
 from functools import partial
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
 import torch
 from func_to_script import script
-from pytorch_accelerated import notebook_launcher
-from pytorch_accelerated.callbacks import get_default_callbacks, SaveBestModelCallback
+from PIL import Image
+from pytorch_accelerated.callbacks import SaveBestModelCallback, get_default_callbacks
 from pytorch_accelerated.schedulers import CosineLrScheduler
-from torch import nn
-
-from example.cars_data_utils import DatasetAdaptor, load_cars_df
+from torch.utils.data import Dataset
 from yolov7 import create_yolov7_model
 from yolov7.dataset import (
-    create_base_transforms,
     Yolov7Dataset,
+    create_base_transforms,
     create_yolov7_transforms,
     yolov7_collate_fn,
 )
@@ -21,19 +22,107 @@ from yolov7.evaluation import CalculateMeanAveragePrecisionCallback
 from yolov7.loss_factory import create_yolov7_loss
 from yolov7.mosaic import MosaicMixupDataset, create_post_mosaic_transform
 from yolov7.trainer import Yolov7Trainer, filter_eval_predictions
-from yolov7.utils import SaveFirstBatchCallback
+from yolov7.utils import SaveBatchesCallback
+
+
+def load_cars_df(annotations_file_path, images_path):
+    all_images = sorted(set([p.parts[-1] for p in images_path.iterdir()]))
+    image_id_to_image = {i: im for i, im in enumerate(all_images)}
+    image_to_image_id = {v: k for k, v, in image_id_to_image.items()}
+
+    annotations_df = pd.read_csv(annotations_file_path)
+    annotations_df.loc[:, "class_name"] = "car"
+    annotations_df.loc[:, "has_annotation"] = True
+
+    # add 100 empty images to the dataset
+    empty_images = sorted(set(all_images) - set(annotations_df.image.unique()))
+    non_annotated_df = pd.DataFrame(list(empty_images)[:100], columns=["image"])
+    non_annotated_df.loc[:, "has_annotation"] = False
+    non_annotated_df.loc[:, "class_name"] = "background"
+
+    df = pd.concat((annotations_df, non_annotated_df))
+
+    class_id_to_label = dict(
+        enumerate(df.query("has_annotation == True").class_name.unique())
+    )
+    class_label_to_id = {v: k for k, v in class_id_to_label.items()}
+
+    df["image_id"] = df.image.map(image_to_image_id)
+    df["class_id"] = df.class_name.map(class_label_to_id)
+
+    file_names = tuple(df.image.unique())
+    random.seed(42)
+    validation_files = set(random.sample(file_names, int(len(df) * 0.2)))
+    train_df = df[~df.image.isin(validation_files)]
+    valid_df = df[df.image.isin(validation_files)]
+
+    lookups = {
+        "image_id_to_image": image_id_to_image,
+        "image_to_image_id": image_to_image_id,
+        "class_id_to_label": class_id_to_label,
+        "class_label_to_id": class_label_to_id,
+    }
+    return train_df, valid_df, lookups
+
+
+class DatasetAdaptor(Dataset):
+    def __init__(
+        self,
+        images_dir_path,
+        annotations_dataframe,
+        transforms=None,
+    ):
+        self.images_dir_path = Path(images_dir_path)
+        self.annotations_df = annotations_dataframe
+        self.transforms = transforms
+
+        self.image_idx_to_image_id = {
+            idx: image_id
+            for idx, image_id in enumerate(self.annotations_df.image_id.unique())
+        }
+        self.image_id_to_image_idx = {
+            v: k for k, v, in self.image_idx_to_image_id.items()
+        }
+
+    def __len__(self) -> int:
+        return len(self.image_idx_to_image_id)
+
+    def __getitem__(self, index):
+        image_id = self.image_idx_to_image_id[index]
+        image_info = self.annotations_df[self.annotations_df.image_id == image_id]
+        file_name = image_info.image.values[0]
+        assert image_id == image_info.image_id.values[0]
+
+        image = Image.open(self.images_dir_path / file_name).convert("RGB")
+        image = np.array(image)
+
+        image_hw = image.shape[:2]
+
+        if image_info.has_annotation.any():
+            xyxy_bboxes = image_info[["xmin", "ymin", "xmax", "ymax"]].values
+            class_ids = image_info["class_id"].values
+        else:
+            xyxy_bboxes = np.array([])
+            class_ids = np.array([])
+
+        if self.transforms is not None:
+            transformed = self.transforms(
+                image=image, bboxes=xyxy_bboxes, labels=class_ids
+            )
+            image = transformed["image"]
+            xyxy_bboxes = np.array(transformed["bboxes"])
+            class_ids = np.array(transformed["labels"])
+
+        return image, xyxy_bboxes, class_ids, image_id, image_hw
 
 
 @script
 def main(
-    data_path: str = "../data/cars",
+    data_path: str = "./data/cars",
     image_size: int = 640,
     pretrained: bool = True,
     num_epochs: int = 30,
 ):
-
-    pretrained = False
-    num_epochs = 300
 
     data_path = Path(data_path)
     images_path = data_path / "training_images"
@@ -42,10 +131,7 @@ def main(
     train_df, valid_df, lookups = load_cars_df(annotations_file_path, images_path)
 
     train_ds = DatasetAdaptor(
-        images_path,
-        train_df,
-        transforms=create_base_transforms(image_size)
-        # transforms=create_base_transforms(1280)
+        images_path, train_df, transforms=create_base_transforms(image_size)
     )
     eval_ds = DatasetAdaptor(images_path, valid_df)
 
@@ -74,23 +160,11 @@ def main(
         architecture="yolov7", num_classes=num_classes, pretrained=pretrained
     )
 
-    conv_weights = {
-        v.weight
-        for k, v in model.named_modules()
-        if (
-            hasattr(v, "weight")
-            and isinstance(v.weight, nn.Parameter)
-            and not isinstance(v, nn.BatchNorm2d)
-        )
-    }
+    param_groups = model.get_parameter_groups()
 
-    other_params = [p for p in model.parameters() if p not in conv_weights]
-
-    lr = 0.01
-    optimizer = torch.optim.SGD(other_params, lr=lr, momentum=0.937, nesterov=True)
-
-    # lr = 0.001
-    # optimizer = torch.optim.AdamW(other_params, lr=lr)
+    optimizer = torch.optim.SGD(
+        param_groups["other_params"], lr=0.01, momentum=0.937, nesterov=True
+    )
 
     loss_func = create_yolov7_loss(model, image_size=image_size)
 
@@ -114,15 +188,16 @@ def main(
                 iou_threshold=0.2,
             ),
             SaveBestModelCallback(watch_metric="map", greater_is_better=True),
-            SaveFirstBatchCallback("./batches", num_images_per_batch=3),
+            SaveBatchesCallback("./batches", num_images_per_batch=3),
             *get_default_callbacks(progress_bar=True),
         ],
     )
 
-    nbs = 64  # nominal batch size
     total_batch_size = (
         batch_size * trainer._accelerator.num_processes
     )  # batch size across all processes
+
+    nbs = 64  # nominal batch size
     num_accumulate_steps = max(
         round(nbs / total_batch_size), 1
     )  # calculate num accum steps
@@ -130,17 +205,18 @@ def main(
     weight_decay *= total_batch_size * num_accumulate_steps / nbs  # scale weight_decay
 
     optimizer.add_param_group(
-        {"params": list(conv_weights), "weight_decay": weight_decay}
+        {"params": param_groups["conv_weights"], "weight_decay": weight_decay}
     )  # add pg1 with weight_decay
 
     trainer.train(
         num_epochs=num_epochs,
         train_dataset=train_yds,
-        # train_dataloader_kwargs={"num_workers": 0},
         eval_dataset=eval_yds,
         per_device_batch_size=batch_size,
         create_scheduler_fn=CosineLrScheduler.create_scheduler_fn(
-            num_warmup_epochs=5, num_cooldown_epochs=cooldown_epochs
+            num_warmup_epochs=5,
+            num_cooldown_epochs=cooldown_epochs,
+            k_decay=2,
         ),
         collate_fn=yolov7_collate_fn,
         gradient_accumulation_steps=num_accumulate_steps,
@@ -150,4 +226,3 @@ def main(
 if __name__ == "__main__":
     os.environ["mixed_precision"] = "fp16"
     main()
-    # notebook_launcher(main, num_processes=2)
